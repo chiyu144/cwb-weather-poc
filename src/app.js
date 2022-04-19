ead2b4b13b49b64f6343bedc3740b6b36509cde8
Chi-Wei Yang <phillisping@gmail.com>
2022-04-20 07:25:43 +0800

    first version with 2 days api

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { getRainfallApi } from './apis.js';
+import { getRainfallApi, getForecastTwoDaysApi } from './apis.js';
 import './styles/global.css';
 
 const getRainfall = async () => {
@@ -6,7 +6,18 @@ const getRainfall = async () => {
   return res.records;
 };
 
+const getForecastTwoDays = async (location) =>{
+  let params, records, data;
+  params = {locationName:location, elementName:['Wx', 'T']};
+  const res = await getForecastTwoDaysApi(params);
+  records = res.records.locations[0].location[0].weatherElement;
+  data = {"Wx":records[0], "T":records[1]};
+  return data;
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   const rainfallRecords = await getRainfall();
   console.log('test', rainfallRecords);
 });
+
+await getForecastTwoDays();
